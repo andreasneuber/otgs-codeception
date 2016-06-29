e8bc680160831eec3d2bd532267dfe05b810f435
Dimitris Mitsis <dimitris.m@icanlocalize.com>
2016-06-29 12:18:30 +0300

    Added logic for checking after a test is completed if debug.log has been altered and to fail the test if so.

--- src/CheckDebugLog.php
+++ src/CheckDebugLog.php
@@ -3,14 +3,25 @@
 namespace Codeception\Module;
 
 use Codeception\Module;
+use Codeception\Module\Asserts;
+use Codeception\Module\Filesystem;
+use Codeception\TestInterface;
 
 
 class CheckDebugLog extends Module
 {
-
-    public function check()
-    {
-        $this->debug("Hello Dim!");
+    public function _initialize() {
+      if ( $this->config['WPpath'] == "" ) {
+        $this->config['WPpath'] = "/";
+      }
     }
 
+    public function _after ( TestInterface $step ) {
+        if ( file_exists($this->config['WPpath'] . "wp-content/debug.log" ) ) {
+            copy( $this->config['WPpath'] . "wp-content/debug.log", "codeception/_output/wpdebug.log" );
+            unlink( $this->config['WPpath'] . "wp-content/debug.log" );
+
+            $this->fail("Debug.log has been altered. Test failed.");
+        }
+    }
 }
