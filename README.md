aa740c3e156a228dd50ad0eac0ae7a293262306c
Konstantinos Kouratoras <kouratoras@gmail.com>
2017-03-28 12:45:33 +0300

    Add function to check debug status (#8)
    * Add function to check debug status
    
    * Update README.md

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
-# codeception-debug-log
-### Modules:  
-#### CheckDebugLog  
-Checks if debug.log of WordPress has been created between tests. If yes, it copies the file to *codeception/_output*  
-
-#### WPCliDb:  
-Uses wp-cli commands to reset, import, replace URLs and handle transients for database dumps after those are imported to a WordPress test site.  
-Also, exports a db dump in case tests fail.
-
+# OnTheGoSystems Codeception Modules
+## CheckDebugLog  
+This module has two options:
+- If `debug` is enabled, it sets a flag to check for errors or notices on page load.
+- If `debugLog` is enabled, it checks if `debug.log` of WordPress installation has been altered between tests. If yes, it copies the file to the output folder of Codeception.  
 ### Example usage in codeception.yml:
 ```
-CheckDebugLog:   
+CheckDebugLog:
+    debug: true
+    debugLog: true
     WPpath: ''
+```
+## WPCliDb:  
+Uses wp-cli commands to reset, import, replace URLs and handle transients for database dumps after those are imported to a WordPress test site.  
+Also, exports a db dump in case tests fail.
+### Example usage in codeception.yml:
+```
 WPCliDb:   
     dump: codeception/_data/dump.sql
     cleandump: codeception/_data/cleandb.sql
@@ -19,9 +23,7 @@ WPCliDb:
     oldUrl: 'http://old.dev'
     newUrl: 'http://new.dev'
 ```
-
 Those dumps can be loaded at the beginning of a test as:  
-
 ```
 start_with_a_clean_site() //loads "cleandump"
 start_with_dump1() //loads "dump1"
--- src/CheckDebugLog.php
+++ src/CheckDebugLog.php
@@ -3,31 +3,33 @@
 namespace Codeception\Module;
 
 use Codeception\Module;
-use Codeception\Module\Asserts;
 use Codeception\TestInterface;
 
-class CheckDebugLog extends Module
-{
-    public function _initialize()
-    {
-        if (!isset($this->config['WPpath'])) {
-            $this->config['WPpath'] = "";
-        }
-    }
+class CheckDebugLog extends Module {
+	private $debug_status;
+	private $debug_log_status;
+	private $debug_log_path;
 
-    public function _before(TestInterface $step) {
-        if ( file_exists($this->config['WPpath'] . "wp-content/debug.log" )) {
-            unlink($this->config['WPpath'] . "wp-content/debug.log");
-        }
-    }
+	public function _initialize() {
+		$this->debug_status     = ( isset( $this->config['debug'] ) ) ? (bool) $this->config['debug'] : false;
+		$this->debug_log_status = ( isset( $this->config['debugLog'] ) ) ? (bool) $this->config['debugLog'] : false;
+		$this->debug_log_path   = ( isset( $this->config['WPPath'] ) ) ? $this->config['WPPath'] . 'wp-content/debug.log' : '../../debug.log';
+	}
 
-    public function _after(TestInterface $step)
-    {
-        if (file_exists($this->config['WPpath'] . "wp-content/debug.log")) {
-            copy($this->config['WPpath'] . "wp-content/debug.log", codecept_output_dir() . "/wpdebug.log");
-            unlink($this->config['WPpath'] . "wp-content/debug.log");
+	public function _before( TestInterface $step ) {
+		if ( $this->debug_log_status == true && file_exists( $this->debug_log_path ) ) {
+			unlink( $this->debug_log_path );
+		}
+	}
 
-            $this->fail("Debug.log has been altered. Test failed.");
-        }
-    }
+	public function _after( TestInterface $step ) {
+		if ( $this->debug_log_status == true && file_exists( $this->debug_log_path ) ) {
+			copy( $this->debug_log_path, codecept_output_dir() . '/debug.log' );
+			$this->fail( "Debug log has been altered. Test failed." );
+		}
+	}
+
+	public function debugIsEnabled() {
+		return (bool) $this->debug_status;
+	}
 }
